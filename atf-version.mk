89493584cec4b8c4b37589b3b02837d6d5a3ae5a
sbwml <admin@cooluc.com>
2026-05-23 02:33:09 +0800

    rk3576: update mainline ATF to v2.14.2
    Signed-off-by: sbwml <admin@cooluc.com>

--- atf-version.mk
+++ atf-version.mk
@@ -9,7 +9,7 @@ RK3399_LOADER:=rk3399_miniloader_v1.30.bin
 RK3568_ATF:=rk3568_bl31_v1.46.elf
 RK3568_DDR:=rk3568_ddr_1560MHz_v1.25.bin
 
-RK3576_ATF:=rk3576_bl31_v2.14.0.elf
+RK3576_ATF:=rk3576_bl31_v2.14.2.elf
 RK3576_DDR:=rk3576_ddr_lp4_2112MHz_lp5_2736MHz_v1.12.bin
 
 RK3588_ATF:=rk3588_bl31_v1.54.elf
